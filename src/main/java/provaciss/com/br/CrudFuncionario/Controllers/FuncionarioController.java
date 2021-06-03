44615c593f9da93b9b9989fedf5406394dfa9ac3
Juan <juanhs8@hotmail.com>
2021-06-03 13:20:21 -0300

    validação

--- src/main/java/provaciss/com/br/CrudFuncionario/Controllers/FuncionarioController.java
+++ src/main/java/provaciss/com/br/CrudFuncionario/Controllers/FuncionarioController.java
@@ -3,13 +3,19 @@ package provaciss.com.br.CrudFuncionario.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import provaciss.com.br.CrudFuncionario.Models.Funcionario;
 import provaciss.com.br.CrudFuncionario.Repositories.FuncionarioRepository;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Validated
 @RestController
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
@@ -30,23 +36,33 @@ public class FuncionarioController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Funcionario createFuncionario(@RequestBody @Valid Funcionario funcionario) {
-        return repository.save(funcionario);
+    public ResponseEntity<Funcionario> createFuncionario(@Valid @RequestBody Funcionario funcionario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+        repository.save(funcionario));
     }
 
     @PutMapping
-    public ResponseEntity updateById(@PathVariable Long id, @RequestBody @Valid Funcionario funcionario) {
-        return repository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity updateFuncionario(@RequestBody @Valid Funcionario funcionario) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                repository.save(funcionario));
     }
 
-    @DeleteMapping
-    public ResponseEntity deleteById(@PathVariable Long id) {
+    @DeleteMapping(path = {"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteById(@PathVariable Long id) {
         repository.deleteById(id);
-        return ResponseEntity.ok().body("deleted");
     }
 
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
--- src/main/java/provaciss/com/br/CrudFuncionario/Models/Funcionario.java
+++ src/main/java/provaciss/com/br/CrudFuncionario/Models/Funcionario.java
@@ -1,14 +1,20 @@
 package provaciss.com.br.CrudFuncionario.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "funcionario")
 public class Funcionario {
     @Id
@@ -26,5 +32,6 @@ public class Funcionario {
     @Email(message = "Email deve ser valido")
     private String email;
 
+    @
     private Long pis;
 }
